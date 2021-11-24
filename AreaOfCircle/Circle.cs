9ffc32859237fe5ff6c34ecf905b5e3b5ba4c15e
pallavi <pallavimatkar26@gmail.com>
2021-11-23 22:28:39 -0600

    area of circle

---
+++ AreaOfCircle/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace AreaOfCircle
+{
+   
+    public static class Circle
+    {
+
+        public static double CalcArea(double radius)
+        {
+            double value = Math.Pow(radius, 2);
+            return Math.PI * value;
+
+            
+        }
+        public static double Circumference(double radius)
+        {
+            
+            return 2 * Math.PI * radius;
+
+
+        }
+        public static double CalcDiameter(double radius)
+        {
+           
+            return 2 * radius;
+
+
+        }
+
+    }
+}
---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius:");
+            string input = Console.ReadLine();
+            double radius = double.Parse(input);
+            double circumference = Circle.Circumference(radius);
+            Console.WriteLine("Please enter miles per gallon");
+            double miles = double.Parse(Console.ReadLine());
+            Console.WriteLine("It will take " + circumference/miles + "gallons of gas to go around circles");
+
+
+        }
+    }
+}
